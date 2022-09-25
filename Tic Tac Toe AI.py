e3e1c33ad3b10c4e0e238c563dbaf008313e4977
Sanskar Jadhav <91966107+sjadhav22@users.noreply.github.com>
2022-09-25 23:11:12 +0530

    Add files via upload

---
+++ Tic Tac Toe AI.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## Tic Tac Toe - Optimal Win Strategy AI with Minimax Backtracking
+
+# An improved modification of the original code credited to 'javacodingcommunity'
+
+# Improvements include:
+# - running the game in a loop (play again feature)
+# - checks for win before draw (in the event that the last move leads to a victory)
+# - ends game when a win or draw occurs (instead of asking user to enter position)
+# - switches alternately between computer and user for who starts first
+
+# In[1]:
+
+
+# I am Sanskar Jadhav, an undergraduate student pursuing a degree in AI and ML
+# I worked on this code as a side project to learn more about AI and for personal experience
+
+
+# In[2]:
+
+
+global board
+global g
+global r
+g = 0
+r = 'y'
+
+def printBoard(board):
+    print(board[1] + '|' + board[2] + '|' + board[3])
+    print('-+-+-')
+    print(board[4] + '|' + board[5] + '|' + board[6])
+    print('-+-+-')
+    print(board[7] + '|' + board[8] + '|' + board[9])
+    print("\n")
+
+
+def spaceIsFree(position):
+    if board[position] == ' ':
+        return True
+    else:
+        return False
+
+
+def insertLetter(letter, position):
+    if spaceIsFree(position):
+        board[position] = letter
+        printBoard(board)
+        if checkForWin():
+            if letter == 'X':
+                print("Bot wins!")
+            else:
+                print("Player wins!")
+        if checkDraw():
+            print("Draw!")
+        return
+    else:
+        print("Can't insert there!")
+        position = int(input("Please enter new position:  "))
+        insertLetter(letter, position)
+        return
+
+
+def checkForWin():
+    if (board[1] == board[2] and board[1] == board[3] and board[1] != ' '):
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] != ' '):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] != ' '):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] != ' '):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] != ' '):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] != ' '):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] != ' '):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] != ' '):
+        return True
+    else:
+        return False
+
+
+def checkWhichMarkWon(mark):
+    if board[1] == board[2] and board[1] == board[3] and board[1] == mark:
+        return True
+    elif (board[4] == board[5] and board[4] == board[6] and board[4] == mark):
+        return True
+    elif (board[7] == board[8] and board[7] == board[9] and board[7] == mark):
+        return True
+    elif (board[1] == board[4] and board[1] == board[7] and board[1] == mark):
+        return True
+    elif (board[2] == board[5] and board[2] == board[8] and board[2] == mark):
+        return True
+    elif (board[3] == board[6] and board[3] == board[9] and board[3] == mark):
+        return True
+    elif (board[1] == board[5] and board[1] == board[9] and board[1] == mark):
+        return True
+    elif (board[7] == board[5] and board[7] == board[3] and board[7] == mark):
+        return True
+    else:
+        return False
+
+
+def checkDraw():
+    for key in board.keys():
+        if (board[key] == ' '):
+            return False
+    return True
+
+
+def playerMove():
+    position = int(input("Enter the position for 'O':  "))
+    insertLetter(player, position)
+    return
+
+
+def compMove():
+    bestScore = -800
+    bestMove = 0
+    for key in board.keys():
+        if (board[key] == ' '):
+            board[key] = bot
+            score = minimax(board, 0, False)
+            board[key] = ' '
+            if (score > bestScore):
+                bestScore = score
+                bestMove = key
+
+    insertLetter(bot, bestMove)
+    return
+
+
+def minimax(board, depth, isMaximizing):
+    if (checkWhichMarkWon(bot)):
+        return 1
+    elif (checkWhichMarkWon(player)):
+        return -1
+    elif (checkDraw()):
+        return 0
+
+    if (isMaximizing):
+        bestScore = -800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = bot
+                score = minimax(board, depth + 1, False)
+                board[key] = ' '
+                if (score > bestScore):
+                    bestScore = score
+        return bestScore
+
+    else:
+        bestScore = 800
+        for key in board.keys():
+            if (board[key] == ' '):
+                board[key] = player
+                score = minimax(board, depth + 1, True)
+                board[key] = ' '
+                if (score < bestScore):
+                    bestScore = score
+        return bestScore
+
+print("Play Tic Tac Toe against my AI! Good luck.")
+print("Positions are as follow:")
+print("1, 2, 3 ")
+print("4, 5, 6 ")
+print("7, 8, 9 ")
+print("\n")
+player = 'O'
+bot = 'X'
+
+g = 0
+r = 'y'
+
+while(r!='n'):
+    board = {1: ' ', 2: ' ', 3: ' ',
+             4: ' ', 5: ' ', 6: ' ',
+             7: ' ', 8: ' ', 9: ' '}
+    printBoard(board)
+    if(g%2==0):
+        while True:
+            if not checkForWin() and not checkDraw():
+                playerMove()
+            else:
+                break
+            if not checkForWin() and not checkDraw():
+                compMove()
+            else:
+                break
+    else:
+        while True:
+            if not checkForWin() and not checkDraw():
+                compMove()
+            else:
+                break
+            if not checkForWin() and not checkDraw():
+                playerMove()
+            else:
+                break
+    g+=1
+    r = input('Play again? y/n ')
+
